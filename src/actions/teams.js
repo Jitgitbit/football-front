36368351b378e916ae4a56f77f54fcba0fda2159
Jitgitbit <rowowolf@gmail.com>
2020-02-07 14:51:14 +0100

    actions

---
+++ src/actions/teams.js
@@ -0,0 +1,91 @@
+import request from 'superagent'
+
+export const TEAMS_FETCHED = 'TEAMS_FETCHED'
+export const TEAM_FETCHED = 'TEAM_FETCHED'
+export const TEAM_CREATE_SUCCESS = 'TEAM_CREATE_SUCCESS'
+export const TEAM_DELETE_SUCCESS = 'TEAM_DELETE_SUCCESS'
+export const TEAM_UPDATE_SUCCESS = 'TEAM_UPDATE_SUCCESS'
+
+const baseUrl = 'http://localhost:4000'
+
+const teamsFetched = teams => ({
+  type: TEAMS_FETCHED,
+  teams
+})
+
+const teamFetched = team => ({
+  type: TEAM_FETCHED,
+  team
+})
+
+const teamCreateSuccess = team => ({
+  type: TEAM_CREATE_SUCCESS,
+  team
+})
+
+const teamDeleteSuccess = team => ({
+  type: TEAM_DELETE_SUCCESS,
+  team
+})
+
+const teamUpdateSuccess = team => ({
+  type: TEAM_UPDATE_SUCCESS,
+  team
+})
+
+
+
+export const deleteEvent = (team) => (dispatch) => {
+  request
+    .delete(`${baseUrl}/teams/${team.id}`)
+    .then(response => {
+      if(response.ok){
+        // passing the original team object because the api 
+        // returns {} instead of the deled record
+        dispatch(teamDeleteSuccess(team))
+      }
+    })
+}
+
+export const loadEvents = () => (dispatch, getState) => {
+  // when the state already contains teams, we don't fetch them again
+  if (getState().teams.length > 0) return
+
+  // a GET /teams request
+  request(`${baseUrl}/teams`)
+    .then(response => {
+      // dispatch an TEAMS_FETCHED action that contains the teams
+      dispatch(teamsFetched(response.body))
+    })
+    .catch(console.error)
+}
+
+export const loadTeam = (id) => (dispatch) => {
+  request(`${baseUrl}/teams/${id}`)
+    .then(response => {
+      dispatch(teamFetched(response.body))
+    })
+    .catch(console.error)
+}
+
+
+
+export const createTeam = (data) => dispatch => {
+  request
+    .post(`${baseUrl}/teams`)
+    .send(data)
+    .then(response => {
+      dispatch(teamCreateSuccess(response.body))
+    })
+    .catch(console.error)
+}
+
+export const updateTeam = (id, data) => dispatch => {
+  request
+    .patch(`${baseUrl}/teams/${id}`)
+    .send(data)
+    .then(response => {
+      dispatch(teamUpdateSuccess(response.body))
+    })
+    .catch(console.error)
+}
---
+++ src/reducers/teams.js
@@ -0,0 +1,22 @@
+import { TEAMS_FETCHED, TEAM_CREATE_SUCCESS, TEAM_DELETE_SUCCESS, TEAM_UPDATE_SUCCESS } from '../actions/TEAMS.js'
+
+export default (state = [], action) => {
+    switch (action.type) {
+        case TEAMS_FETCHED:
+            return action.teams
+        case TEAM_CREATE_SUCCESS:
+            return [...state, action.TEAM]
+        case TEAM_DELETE_SUCCESS:
+            return state.filter(team => team.id !== action.team.id)
+        case TEAM_UPDATE_SUCCESS:
+            return state.map(team => {
+                if(team.id === action.team.id){
+                    return action.team
+                }
+                return team
+            })
+    
+        default:
+            return state
+    }
+}
